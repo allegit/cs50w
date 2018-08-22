e30f6c53a1295d48d9617c6f2e642d6286b1268f
Vasanth <41694566+allegit@users.noreply.github.com>
2018-08-22 14:48:31 -0400

    modified sequences
    modified sequences to print other variables

--- sequences.py
+++ sequences.py
@@ -3,3 +3,5 @@ coordinates = (100.0, 200.0)
 names = ["Poorna", "Vasanth", "Kartik", "Ritvik"]
 
 print(f"First char {name[0]}")
+print(f"coordinates is {coordinates[0]}")
+print (f"First name in Array names, {names[0]}")
