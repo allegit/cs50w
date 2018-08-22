bb0ef7988be5edcbe4ed783febf3d5241ad1e424
Vasanth <41694566+allegit@users.noreply.github.com>
2018-08-22 14:56:36 -0400

    More data types
    Sets, dictionaries

--- sequences.py
+++ sequences.py
@@ -5,3 +5,16 @@ names = ["Poorna", "Vasanth", "Kartik", "Ritvik"]
 print(f"First char {name[0]}")
 print(f"coordinates is {coordinates[0]}")
 print (f"First name in Array names, {names[0]}")
+
+s = set()
+s.add(1)
+s.add(5)
+s.add(8)
+s.add(5)
+print(s)
+
+ages = {"Poorna": 39, "Ritvik": 11, "Kartik": 14}
+ages["Vasanth"] = 46
+ages["Ritvik"] += 1
+
+print(ages)
