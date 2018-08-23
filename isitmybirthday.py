04e4efe4d23de2473e21d117dd8b9302ee0f067a
Vasanth <41694566+allegit@users.noreply.github.com>
2018-08-23 15:02:16 -0400

    added app.run to isitmybirthday.py

--- isitmybirthday.py
+++ isitmybirthday.py
@@ -9,3 +9,6 @@ def index():
     now = datetime.datetime.now()
     mybirthday = now.month == 8 and now.day == 7
     return render_template("index.html", mybirthday=mybirthday)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")    
