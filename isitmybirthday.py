2e0986e507ea62b666693e1acf2b89e4e13c82e3
Vasanth <41694566+allegit@users.noreply.github.com>
2018-08-23 15:12:14 -0400

    change to variable mybirthday to my_birthday

--- isitmybirthday.py
+++ isitmybirthday.py
@@ -7,8 +7,8 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     now = datetime.datetime.now()
-    mybirthday = now.month == 8 and now.day == 7
-    return render_template("index.html", mybirthday=mybirthday)
+    my_birthday = now.month == 8 and now.day == 7
+    return render_template("index.html", my_birthday=my_birthday)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")    
+    app.run(host="0.0.0.0")
