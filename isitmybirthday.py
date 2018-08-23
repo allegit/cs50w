1207a9fdd0636340ec518c73331e919be8c517de
Vasanth <41694566+allegit@users.noreply.github.com>
2018-08-23 15:00:31 -0400

    mybirthday python file
    Check if today is my birthday

---
+++ isitmybirthday.py
@@ -0,0 +1,11 @@
+import datetime
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    now = datetime.datetime.now()
+    mybirthday = now.month == 8 and now.day == 7
+    return render_template("index.html", mybirthday=mybirthday)
