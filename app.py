72c560948173d0e7e2acfddd73ebf71cf407d8ea
Vasanth <41694566+allegit@users.noreply.github.com>
2018-08-23 07:26:18 -0400

    Added route Alle

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def hello():
     return "hello world!"
 
+@app.route("/alle")
+def alle():
+    return "Welcome Alle!"
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
