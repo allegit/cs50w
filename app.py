2b8568cda1042893d6b8ff78670fa77ac11ea918
Vasanth <41694566+allegit@users.noreply.github.com>
2018-08-23 07:33:08 -0400

    Added h1 tag

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ def hello():
 @app.route("/<string:name>")
 def welcome(name):
     name = name.capitalize()
-    return f"Welcome, {name}!"
+    return f"<h1>Welcome, {name}!</h1>"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
