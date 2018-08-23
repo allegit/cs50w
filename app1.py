aec584182323a228c9b62e872fde31c7b4505da9
Vasanth <41694566+allegit@users.noreply.github.com>
2018-08-23 14:55:59 -0400

    Added file, index.html, modified app1
    Added a new index.html that contains a Jinja syntax in html
    
    Modifed app1.py to replace the tag in index.html

--- app1.py
+++ app1.py
@@ -6,5 +6,10 @@ app = Flask(__name__)
 def index():
     return render_template("links1.html")
 
+@app.route("/bye")
+def bye():
+    headline = "Goodbye!"
+    return render_template("index.html", headline=headline)
+    
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
