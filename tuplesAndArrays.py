1d0f9c38025a0d5b6baf18e7e9f8ce73d51bed37
Vasanth <41694566+allegit@users.noreply.github.com>
2018-09-05 15:11:42 -0400

    Arrays study

---
+++ tuplesAndArrays.py
@@ -0,0 +1,22 @@
+#Array
+from array import array
+
+a1 = array('l')
+a2 = array('l', [10,20,30,100,400,500])
+a3 = array('u', 'Hello')
+a4 = array('d', [3.14159, 2.71828, 1.41421, 1.61803])
+
+print(f'Before pop: {a2}')
+a2.pop()
+print(f'After pop: {a2}')
+
+a3.append('!')
+print(f'{a3}')
+a3.insert(5, '?')
+print(f'{a3}')
+
+print('a4[0]: ' + str(a4[0]))
+
+print()
+
+#tuples
