126932d370b67d49060c70fd1285c99685da949c
Vasanth <41694566+allegit@users.noreply.github.com>
2018-09-05 14:44:27 -0400

    Strings and printing

---
+++ format.py
@@ -0,0 +1,29 @@
+#format
+print("There are " + str(10) + " fish in the barrel.")
+print("There are {} fish in the barrel.".format(10))
+
+#f-strings
+start-date = "2018-01-01"
+print(f"Start date: {start_date}")
+
+print("""
+This
+is
+a
+multi-line
+string.
+""")
+
+#escaping
+#carriage returns
+print("1\n2\n3")
+
+#tabs
+print("1\t2\t3")
+
+#backslash
+print("Escaped backslash: \\")
+
+#Quotes
+print('It\'s a single quote.')
+print("Double Quotes \"\"")
