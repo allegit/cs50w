d765e40d655a9dd1227366d39e2cbdf7a06d56af
allegit <vasanth.alle@gmail.com>
2020-02-05 22:46:05 -0500

    Create openFileHash.py
    This is a hash version of the openFile, a hash is used to associate names with scores, with scores as the keys to this hash.

---
+++ openFileHash.py
@@ -0,0 +1,19 @@
+"""openFileHash.py
+
+Reads a file containing scores then prints the data as-is, sorts and prints and then reverses
+and prints. This program uses a hash to keep the names and score together
+"""
+
+scores = {}					# Hash to hold scores and names
+
+result_f = open('results.txt')
+for line in result_f:
+	(name,score) = line.split()
+	scores[score] = name
+
+result_f.close()
+print("The contents of the results file:")
+
+for each_score in scores.keys(): 
+	print('Surfer ' + scores[each_score] + ' scored ' + each_score)
+
