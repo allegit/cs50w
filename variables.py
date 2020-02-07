ca3e6372ae0828a6db9e44f69f901b7e8564b3e3
allegit <vasanth.alle@gmail.com>
2020-02-06 23:43:49 -0500

    All about python variables

---
+++ variables.py
@@ -0,0 +1,32 @@
+'''
+Example to declare variables
+'''
+
+f=100
+print(f)
+
+'Re-initialize f'
+
+f='CP4MCM'
+print(f)
+
+a="Guru"
+b=95
+print(a+str(b))
+
+def someFunc():
+    f = "This is python programming...."
+    print(f)
+
+someFunc()
+print(f)
+
+'Local variable scope can be changed to global'
+
+def someFunc2():
+    global f
+    print(f)
+    f = 'Python programming is fun...'
+
+someFunc2()
+print(f)
