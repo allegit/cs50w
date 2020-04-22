48c52ac9af685d623b36f56714671d4f6fd3d4b8
VASANTHA K ALLE <allevas@us.ibm.com>
2020-04-22 17:45:01 -0400

    added a few comments...

--- allaboutclass.py
+++ allaboutclass.py
@@ -3,6 +3,9 @@
 '''
 
 class Student:
+
+    # The '__init__' mnethod is used to set other attributes to complete the process of initialization process, but nothing is returned
+    
     def __init__(self,name,age,school,grade):
         print('__init__ method gets called')
         self.name = name
@@ -10,6 +13,10 @@ class Student:
         self.school = school
         self.grade = grade
     
+    # The '__new__' method creates an empty instance object as its returned value of the class
+    # The below definition of __new__ takes in arguments 'cls' - this means an object needs to be created for this class, synonymous of the self
+    # *args method signifies that multiple arguments can be taken by this method.
+
     def __new__(cls,*args):
         print('__new__ method gets called...')
         student = object.__new__(cls)
